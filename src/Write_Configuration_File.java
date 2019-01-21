04659851a63aeed1922e912a9a329eea1bad76a5
Banee Ishaque K <Baneeishaque@gmail.com>
2019-01-21 14:28:05 +0530

    Configuration progress2

--- src/Write_Configuration_File.java
+++ src/Write_Configuration_File.java
@@ -31,40 +31,120 @@ public class Write_Configuration_File {
             // Compare elements
             Element compare = doc.createElement("Compare");
             rootElement.appendChild(compare);
-            
+
             // Variant elements
             Element variant = doc.createElement("Variant");
             variant.appendChild(doc.createTextNode("TimeAndSize"));
             compare.appendChild(variant);
-            
+
             Element Symlinks = doc.createElement("Symlinks");
             Symlinks.appendChild(doc.createTextNode("Exclude"));
             compare.appendChild(Symlinks);
-            
+
             Element IgnoreTimeShift = doc.createElement("IgnoreTimeShift");
             compare.appendChild(IgnoreTimeShift);
-            
+
             // Synchronize elements
             Element Synchronize = doc.createElement("Synchronize");
             rootElement.appendChild(Synchronize);
-            
+
             // Variant elements
             Element variant2 = doc.createElement("Variant");
             variant2.appendChild(doc.createTextNode("Mirror"));
             Synchronize.appendChild(variant2);
-            
+
             Element DetectMovedFiles = doc.createElement("DetectMovedFiles");
             DetectMovedFiles.appendChild(doc.createTextNode("true"));
             Synchronize.appendChild(DetectMovedFiles);
-            
+
             Element DeletionPolicy = doc.createElement("DeletionPolicy");
             DeletionPolicy.appendChild(doc.createTextNode("RecycleBin"));
             Synchronize.appendChild(DeletionPolicy);
-            
+
             Element VersioningFolder = doc.createElement("VersioningFolder");
             VersioningFolder.setAttribute("Style", "Replace");
             Synchronize.appendChild(VersioningFolder);
 
+            // Synchronize elements
+            Element Filter = doc.createElement("Filter");
+            rootElement.appendChild(Filter);
+
+            // Include elements
+            Element Include = doc.createElement("Include");
+            Element Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("*"));
+            Include.appendChild(Item);
+            Filter.appendChild(Include);
+
+            Element Exclude = doc.createElement("Exclude");
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("\\System Volume Information\\"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("\\$Recycle.Bin\\"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("\\RECYCLER\\"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("\\RECYCLED\\"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("*\\desktop.ini"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("*\\thumbs.db"));
+            Exclude.appendChild(Item);
+
+            // Linux
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("/.Trash-*/"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("/.recycle/"));
+            Exclude.appendChild(Item);
+
+            // Mac
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("/.fseventsd/"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("/.Spotlight-V100/"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("/.Trashes/"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("*/.DS_Store"));
+            Exclude.appendChild(Item);
+
+            Item = doc.createElement("Item");
+            Item.appendChild(doc.createTextNode("*/._.*"));
+            Exclude.appendChild(Item);
+
+            Filter.appendChild(Exclude);
+
+//            Element DetectMovedFiles = doc.createElement("DetectMovedFiles");
+//            DetectMovedFiles.appendChild(doc.createTextNode("true"));
+//            Synchronize.appendChild(DetectMovedFiles);
+//            
+//            Element DeletionPolicy = doc.createElement("DeletionPolicy");
+//            DeletionPolicy.appendChild(doc.createTextNode("RecycleBin"));
+//            Synchronize.appendChild(DeletionPolicy);
+//            
+//            Element VersioningFolder = doc.createElement("VersioningFolder");
+//            VersioningFolder.setAttribute("Style", "Replace");
+//            Synchronize.appendChild(VersioningFolder);
 //            // lastname elements
 //            Element lastname = doc.createElement("lastname");
 //            lastname.appendChild(doc.createTextNode("mook kim"));
@@ -79,7 +159,6 @@ public class Write_Configuration_File {
 //            Element salary = doc.createElement("salary");
 //            salary.appendChild(doc.createTextNode("100000"));
 //            staff.appendChild(salary);
-
             // write the content into xml file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
@@ -88,7 +167,6 @@ public class Write_Configuration_File {
 
             // Output to console for testing
 //            StreamResult result = new StreamResult(System.out);
-
             transformer.transform(source, result);
 
             System.out.println("File saved!");
